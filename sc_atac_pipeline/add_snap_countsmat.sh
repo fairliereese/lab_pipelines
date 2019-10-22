660c521e33e4579748895846453c272890f06d33
fairliereese <fairliek@comcast.net>
2019-10-22 15:29:05 -0400

    added binsize option

--- sc_atac_pipeline/add_snap_countsmat.sh
+++ sc_atac_pipeline/add_snap_countsmat.sh
@@ -1,10 +1,12 @@
 # from https://github.com/r3fang/SnapATAC/wiki/FAQs#cellranger_output
 snap=$1
+binsize=$2
 prefix=${snap%.snap}
 echo $snap
 echo $prefix
+echo $binsize
 
 snaptools snap-add-bmat  \
 	--snap-file=${snap}  \
-	--bin-size-list 50  \
+	--bin-size-list ${binsize}  \
 	--verbose=True
