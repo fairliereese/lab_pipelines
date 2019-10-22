8bb4858cca260680451b1d26cee12b1a033c8361
fairliereese <fairliek@comcast.net>
2019-10-22 12:02:43 -0400

    updated snapfile generation script

--- sc_atac_pipeline/generate_snapfile.sh
+++ sc_atac_pipeline/generate_snapfile.sh
@@ -1,12 +1,13 @@
 # from https://github.com/r3fang/SnapATAC/wiki/FAQs#cellranger_output
 bam=$1
 prefix=${bam%.bam}
+mm10_sizes=/home/freese/mortazavi_lab/ref/mm10/mm10.chrom.sizes
 
 snaptools snap-pre  \
 	--input-file=${bam}  \
 	--output-snap=${bam}.snap  \
 	--genome-name=mm10  \
-	--genome-size=mm10.chrom.sizes  \
+	--genome-size=${mm10_sizes}  \
 	--min-mapq=30  \
 	--min-flen=50  \
 	--max-flen=1000  \
