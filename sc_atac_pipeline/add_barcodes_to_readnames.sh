c9c1f45c2bccd906723b2dbeba1f221947df22b0
fairliereese <fairliek@comcast.net>
2019-10-22 11:28:45 -0400

    added bash barcodes to bam

---
+++ sc_atac_pipeline/add_barcodes_to_readnames.sh
@@ -0,0 +1,14 @@
+# from https://github.com/r3fang/SnapATAC/wiki/FAQs#cellranger_output
+bam=$1
+prefix=${bam%.bam}
+echo $bam
+echo $prefix
+exit
+
+# extract the header file
+samtools view ${bam} -H > ${prefix}.header.sam
+
+# create a bam file with the barcode embedded into the read name
+cat <( cat ${prefix}.header.sam ) \
+<( samtools view ${bam} | awk '{for (i=12; i<=NF; ++i) { if ($i ~ "^RG:Z:"){ td[substr($i,1,2)] = substr($i,6,length($i)-5); } }; printf "%s:%s\n", td["RG"], $0 }' ) \
+| samtools view -bS - > ${prefix}.snap.bam
