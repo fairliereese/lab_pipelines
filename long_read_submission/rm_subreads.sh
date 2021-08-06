a6e85d46f41e71c2d2109408db5db40c29a4eeca
fairliereese <fairliek@comcast.net>
2021-08-06 15:12:34 -0700

    added line ending conversion

--- long_read_submission/rm_subreads.sh
+++ long_read_submission/rm_subreads.sh
@@ -1,10 +1,13 @@
 files=$1
 
+dos2unix $files
+
 cat $files | while read p
 do
 	echo $p
 	for f in ~/pacbio/${p}/*01_data/*subreads*
 	do
-		echo $f
+		echo "Removing $f...\n"
+		rm $f
 	done
 done
