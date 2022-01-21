fba7eac3e54c87c05b778cb2f1f0dcd6abee1e89
fairliereese <fairliek@comcast.net>
2022-01-20 16:13:27 -0800

    udpaote

--- splitseq_submission/get_file_ids.py
+++ splitseq_submission/get_file_ids.py
@@ -2,13 +2,19 @@ import pandas as pd
 import os
 import sys
 
-# usage: python get_file_ids.py <encode file submission filename>
+# usage: python get_file_ids.py <encode file submission filename r1> <encode file submission filename r2> <opref>
 
-ifile = sys.argv[1]
-opref = sys.argv[2]
+ifile_r1 = sys.argv[1]
+ifile_r2 = sys.argv[2]
+opref = sys.argv[3]
 
-df = pd.read_csv(ifile, sep='\t')
-
+df = pd.read_csv(ifile_r1, sep='\t')
 df = df['aliases'].to_frame()
+
 ofile = '{}_file_ids.tsv'.format(opref)
 df.to_csv(ofile, sep='\t', index=False)
+
+df = pd.read_csv(ifile_r2, sep='\t')
+df = df['aliases'].to_frame()
+
+df.to_csv(ofile, sep='\t', index=False, mode='a')
